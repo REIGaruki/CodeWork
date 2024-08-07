d1b1c90d0997350b6409f350edb8bd0f3d866581
REIGaruki <gyakujo@mail.ru>
2024-08-07 12:30:07 +0300

    Step 2

--- src/main/java/ru/hogwarts/school/service/AvatarService.java
+++ src/main/java/ru/hogwarts/school/service/AvatarService.java
@@ -2,18 +2,20 @@ package ru.hogwarts.school.service;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.hogwarts.school.repository.AvatarRepository;
 
 @Service
+@Transactional
 public class AvatarService {
-    private final AvatarRepository avatarRepository;
     private final StudentService studentService;
+    private final AvatarRepository avatarRepository;
 
     @Value("${path.to.avatars.folder}")
     private String avatarsDir;
 
-    public AvatarService(AvatarRepository avatarRepository, StudentService studentService) {
-        this.avatarRepository = avatarRepository;
+    public AvatarService(StudentService studentService, AvatarRepository avatarRepository) {
         this.studentService = studentService;
+        this.avatarRepository = avatarRepository;
     }
 }
