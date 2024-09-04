fe0ef471c9e657106ee5276d49fc6fa313c051c8
REIGaruki <gyakujo@mail.ru>
2024-09-04 17:27:38 +0300

    index-practice

--- src/main/resources/liquibase/scripts/index-practice.sql
+++ src/main/resources/liquibase/scripts/index-practice.sql
@@ -1,1 +1,7 @@
+-- liquibase formatted sql
 
+-- changeset agalkin:1
+CREATE INDEX student_name_index ON student (name);
+
+-- changeset agalkin:2
+CREATE INDEX faculty_nc_idx ON faculty (name, color);
