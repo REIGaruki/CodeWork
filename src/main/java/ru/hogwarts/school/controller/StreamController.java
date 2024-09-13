0f4908dcd5612b58b2a6ca0c495fcee1e393056f
REIGaruki <gyakujo@mail.ru>
2024-09-13 10:12:16 +0300

    Streams comparison

--- src/main/java/ru/hogwarts/school/controller/StreamController.java
+++ src/main/java/ru/hogwarts/school/controller/StreamController.java
@@ -3,6 +3,7 @@ package ru.hogwarts.school.controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 import java.util.stream.Stream;
 
 @RestController
@@ -10,13 +11,27 @@ import java.util.stream.Stream;
 public class StreamController {
 
     @GetMapping()
-    public Long getMath(@RequestParam(name="quant", required = false, defaultValue = "1000000") long quant) {
+    public void getMath(@RequestParam(name="quant", required = false, defaultValue = "1000000") int quant) {
         long start = System.currentTimeMillis();
-        IntStream
+        Stream
                 .iterate(1, a -> a++)
                 .limit(quant)
                 .reduce(0, Integer::sum);
         long finish = System.currentTimeMillis();
-        return finish - start;
+        System.out.println("Stream of " + quant + " elemesnts: " + (finish - start) + " ms");
+        long startInt = System.currentTimeMillis();
+        IntStream
+                .range(1, quant + 1)
+                .reduce(0, Integer::sum);
+        long finishInt = System.currentTimeMillis();
+        System.out.println("Integer stream of " + quant + " elemesnts: " + (finishInt - startInt) + " ms");
+        long startPara = System.currentTimeMillis();
+        Stream
+                .iterate(1, a -> a++)
+                .limit(quant)
+                .parallel()
+                .reduce(0, Integer::sum);
+        long finishPara = System.currentTimeMillis();
+        System.out.println("Parallel stream of " + quant + " elemesnts: " + (finishPara - startPara) + " ms");
     }
 }
